89dce550c944b216fa344154c10fafd35b0bbc01
Juan Franco Ledesma <juanfledesma18@gmail.com>
2024-01-20 19:45:00 -0300

    pokemon CRUD

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePokemonDto {}
+import { IsInt, IsString, MinLength, IsPositive, Min } from "class-validator";
+
+export class CreatePokemonDto {
+
+    // isInt, isPositive, min 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    // isString, MinLength 1
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+}
